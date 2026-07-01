8235991452c05ac9d12091138b9cbb77080d9401
Bhuvan Teja Kudupudi <bhuvanteja.kudupudi@gmail.com>
2026-07-01 11:28:06 -0400

    simple password protected system implemented

---
+++ app/community/secure/page.tsx
@@ -0,0 +1,17 @@
+export default function SecureCommunityPage() {
+  return (
+    <div className="container pt-4 pb-5">
+      <h1 className="fw-bold text-primary mb-4">Community Partnership Files</h1>
+
+      <p className="fs-5">
+        You now have access to the protected Indigenous language documentation materials.
+      </p>
+
+      <ul className="mt-4">
+        <li>Document 1</li>
+        <li>Document 2</li>
+        <li>Document 3</li>
+      </ul>
+    </div>
+  );
+}
